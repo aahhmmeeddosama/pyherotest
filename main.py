ef9efffe137f91905cf0e569a1a59eb171342538
aahhmmeeddosama <67119304+aahhmmeeddosama@users.noreply.github.com>
2022-05-21 23:38:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(print_hi('ahmed'))
+    print_hi('ahmed')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
